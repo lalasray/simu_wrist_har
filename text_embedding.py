e3bcf4efe68815b75bd2934174401755275b7b7d
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2024-04-29 19:30:26 +0200

    update

---
+++ text_embedding.py
@@ -0,0 +1,38 @@
+import csv
+from InstructorEmbedding import INSTRUCTOR
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+
+def extract_sentences_from_csv(filename, prefix):
+    sentences = []
+    with open(filename, 'r', newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        for row in csvreader:
+            sentences.append([f"{prefix}:"] + [row[6]])  # Assuming the "SENTENCE" column is at index 6
+    return sentences
+
+csv_filename = r"C:\Users\lalas\Downloads\how2sign_val.csv"
+prefix = "Represent conversational speech for clustering"
+sentences = extract_sentences_from_csv(csv_filename, prefix)
+
+print(len(sentences))
+
+model = INSTRUCTOR('hkunlp/instructor-large')
+
+embeddings = model.encode(sentences)
+
+print(embeddings.shape)
+
+# Reduce dimensionality using t-SNE
+tsne = TSNE(n_components=2, perplexity=30, metric="cosine", init="random")  # Adjust perplexity here
+embeddings_tsne = tsne.fit_transform(embeddings)
+
+# Plot the clusters
+plt.figure(figsize=(8, 6))
+plt.scatter(embeddings_tsne[:, 0], embeddings_tsne[:, 1], cmap='viridis')
+plt.title('t-SNE Visualization of Clusters')
+plt.xlabel('t-SNE Dimension 1')
+plt.ylabel('t-SNE Dimension 2')
+plt.colorbar(label='Cluster')
+plt.show()
---
+++ text_embedding_test.py
@@ -0,0 +1,7 @@
+from InstructorEmbedding import INSTRUCTOR
+
+model = INSTRUCTOR('hkunlp/instructor-large')
+sentence = "3D ActionSLAM: wearable person tracking in multi-floor environments"
+instruction = "Represent the Science title:"
+embeddings = model.encode([[instruction,sentence]])
+print(embeddings.shape)
