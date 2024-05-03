52cd18a7554650d218569bbad6afe9c345c9a047
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2024-05-03 13:43:03 +0200

    Update text_embedding_how2sign.py

--- text/text_embedding_how2sign.py
+++ text/text_embedding_how2sign.py
@@ -3,38 +3,47 @@ from InstructorEmbedding import INSTRUCTOR
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import numpy as np
+
 
 def extract_sentences_from_csv(filename, prefix):
     sentences = []
+    filenames = []
     with open(filename, 'r', newline='', encoding='utf-8') as csvfile:
         csvreader = csv.reader(csvfile, delimiter='\t')
         for row in csvreader:
-            sentences.append([f"{prefix}:"] + [row[6]])  # Assuming the "SENTENCE" column is at index 6
-    return sentences
+            filenames.append(row[3])
+            sentences.append([f"{prefix}:"] + [row[6]])  
+    return sentences, filenames
 
-csv_filename = r"C:\Users\lalas\Downloads\how2sign_val.csv"
+csv_filename = "/home/lala/other/Repos/git/simu_wrist_har/data/how2sign/train/ann.csv"
 prefix = "Represent conversational speech senetence for clustering"
-sentences = extract_sentences_from_csv(csv_filename, prefix)
-
-print(len(sentences))
+sentences, filenames = extract_sentences_from_csv(csv_filename, prefix)
+#print(sentences)
+#print(filenames)
+#print(len(sentences),len(filenames))
 
 model = INSTRUCTOR('hkunlp/instructor-large')
 
 embeddings = model.encode(sentences)
+#print(embeddings.shape[0])
+numpy_array = np.array(filenames)
+#print(numpy_array.shape[0])
 
-print(embeddings.shape)
-
-kmeans = KMeans(n_clusters=5)
+#kmeans = KMeans(n_clusters=5)
 # Reduce dimensionality using t-SNE
-tsne = TSNE(n_components=2, perplexity=30, metric="cosine", init="random")  # Adjust perplexity here
-embeddings_tsne = tsne.fit_transform(embeddings)
-labels = kmeans.fit_predict(embeddings)
+#tsne = TSNE(n_components=2, perplexity=30, metric="cosine", init="random")  # Adjust perplexity here
+#embeddings_tsne = tsne.fit_transform(embeddings)
+#labels = kmeans.fit_predict(embeddings)
 
 # Plot the clusters
-plt.figure(figsize=(8, 6))
-plt.scatter(embeddings_tsne[:, 0], embeddings_tsne[:, 1],c=labels, cmap='viridis')
-plt.title('t-SNE Visualization of Clusters')
-plt.xlabel('t-SNE Dimension 1')
-plt.ylabel('t-SNE Dimension 2')
-plt.colorbar(label='Cluster')
-plt.show()
+#plt.figure(figsize=(8, 6))
+#plt.scatter(embeddings_tsne[:, 0], embeddings_tsne[:, 1],c=labels, cmap='viridis')
+#plt.title('t-SNE Visualization of Clusters')
+#plt.xlabel('t-SNE Dimension 1')
+#plt.ylabel('t-SNE Dimension 2')
+#plt.colorbar(label='Cluster')
+#plt.show()
+new_directory = csv_filename.replace('ann.csv', 'text_processed/')
+for i in range (1,numpy_array.shape[0]):
+    np.save(new_directory+numpy_array[i]+".npy",embeddings[i])
