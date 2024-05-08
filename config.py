7ce5f79c618da0f3d1d39e6fca0a83bba9278827
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2024-05-08 10:56:29 +0200

    up

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ pose_type = "pose" #embedding
 pose_encoder_type = "spatiotemporal"  #fc #cnn #res #spatiotemporal
 
 batch_size = 32
-embedding_dim = 2048
+embedding_dim = 4096
 num_epochs = 300
 patience = 10
 
@@ -16,7 +16,7 @@ classes = 10
 num_epochs_class = 100
 batch_size_class = 32
 
-parent = "c:/Users/lalas/Documents/GitHub/simu_wrist_har/"
-#parent = "/home/lala/other/Repos/git/simu_wrist_har/"
+#parent = "c:/Users/lalas/Documents/GitHub/simu_wrist_har/"
+parent = "/home/lala/other/Repos/git/simu_wrist_har/"
 
 loss = InfonceLoss() #ContrastiveLoss() #InfonceLoss()
